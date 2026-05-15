3fe50a308fa6f458e16e4c8bbd44a659be005744
ANUSHKA MITRA <anushkamitrateam@gmail.com>
2026-05-15 13:15:27 +0530

    Create 1004_Max_Consecutive_Ones_III.cpp

---
+++ Sliding_Window/1004_Max_Consecutive_Ones_III.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        
+        int left=0, zeros=0, maxlen=0;
+        //at most 'k' zeros
+        for(int right=0; right<nums.size(); right++){
+            if(nums[right]==0)  zeros++;
+
+            while(zeros>k){
+                if(nums[left]==0)   zeros--;
+                left++;
+            }
+
+            maxlen=max(maxlen, right-left+1);
+        }
+        return maxlen;
+    }
+};
