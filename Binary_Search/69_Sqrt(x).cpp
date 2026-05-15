ddd7cdf798e0644e4eba99e5e45f215f622d6c52
ANUSHKA MITRA <anushkamitrateam@gmail.com>
2026-05-15 22:56:09 +0530

    Create 69_Sqrt(x).cpp

---
+++ Binary_Search/69_Sqrt(x).cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        long long left=0, right=x, ans=0, mid=0;
+
+        while(left<=right){
+            mid=left+(right-left)/2;
+
+            if(mid*mid==x)  return mid;
+            else if(mid*mid<x){
+                ans=mid; left=mid+1;
+            }   
+            else right=mid-1;
+        }
+        return ans;
+    }
+};
