17799f092117c615234285b162fb04ef272b3bbe
ANUSHKA MITRA <anushkamitrateam@gmail.com>
2026-05-16 18:51:39 +0530

    Create 1011_Capacity_To_Ship_Packages_Within_D_Days.cpp

---
+++ Binary_Search/1011_Capacity_To_Ship_Packages_Within_D_Days.cpp
@@ -0,0 +1,34 @@
+class Solution{
+public:
+    bool canShip(vector<int>& weights, int days, int capacity){
+        int usedDays = 1;
+        int currentWeight = 0;
+        for(int weight : weights){
+            if(currentWeight+weight > capacity){
+                usedDays++;
+                currentWeight=0;
+            }
+            currentWeight+=weight;
+        }
+        return usedDays<=days;
+        }
+        
+    
+
+    int shipWithinDays(vector<int>& weights, int days){
+        int left=*max_element(weights.begin(), weights.end());
+        int right=accumulate(weights.begin(), weights.end(), 0);
+        int ans=right;
+
+        while(left<=right){
+            int mid=left+(right-left)/2;
+
+            if(canShip(weights, days, mid)){
+                ans=mid;
+                right=mid-1;
+            }
+            else    left = mid + 1;
+        }
+        return ans;
+    }
+};
