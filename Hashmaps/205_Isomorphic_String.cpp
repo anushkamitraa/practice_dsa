817dd7a5f7a68546ed39f627cc8404d7c8937a2a
ANUSHKA MITRA <anushkamitrateam@gmail.com>
2026-05-23 11:53:59 +0530

    Implement isIsomorphic function for string comparison

---
+++ Hashmaps/205_Isomorphic_String.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char,char> s2t, t2s;
+        for(int i=0; i<s.size(); i++){
+            char a=s[i], b=t[i];
+            if(s2t.count(a) && s2t[a]!=b)   return false;
+            if(t2s.count(b) && t2s[b]!=a)   return false;
+
+            s2t[a]=b; t2s[b]=a;
+        }
+        return true;
+    }
+};
