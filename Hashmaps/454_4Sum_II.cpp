1311a5443ffc6ed3f544366d6898b6727f25caa8
ANUSHKA MITRA <anushkamitrateam@gmail.com>
2026-05-23 19:45:34 +0530

    Create 454_4Sum_II.cpp

---
+++ Hashmaps/454_4Sum_II.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        unordered_map<int, int> sumAB;
+        for(int a:nums1)
+            for(int b:nums2)
+                sumAB[a+b]++;
+        int cnt=0;
+        for(int c:nums3)    
+            for(int d:nums4)
+                if(sumAB.count(-c-d))   cnt+=sumAB[-c-d];
+        return cnt;
+    }
+};
