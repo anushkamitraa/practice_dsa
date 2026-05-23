8c513b5210bc787ad285ba6e055e0d6dd7a723aa
ANUSHKA MITRA <anushkamitrateam@gmail.com>
2026-05-23 13:40:48 +0530

    Implement intersection of two arrays using hashmap

---
+++ Hashmaps/350_Intersection_of_two_Arrays_II.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> freq;
+        for(int n:nums1)    freq[n]++;
+        vector<int> res;
+        for(int n:nums2){
+            if(freq[n]>0){
+                res.push_back(n); freq[n]--;
+            }
+        }
+        return res;
+    }
+};
